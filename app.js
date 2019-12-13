cec8a52cb810a2e4b8ce570d4fa3f0a659b009c1
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-12-13 16:38:13 +0800

    init

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+// express 4.0 以后不支持 body-parser
 const bodyParser = require('body-parser')
-app.use(bodyParser.json)
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 )
+app.use(bodyParser.json())
+
 app.post('/user/login', (req, res) => {
   console.log(req.body)
   const { username } = req.body
@@ -24,13 +26,33 @@ app.post('/user/login', (req, res) => {
 })
 app.get('/user/info', (req, res) => {
   const auth = req.headers['authorization']
-  const roles = auth.split(' ')[1] === 'admin' ? ['admin'] : ['editor']
-  res.json({
-    code: 1,
-    data: roles
-  })
+  if(auth) {
+    const roles = auth.split(' ')[1] === 'admin' ? ['admin'] : ['editor']
+    res.json({
+      code: 1,
+      data: roles
+    })
+  } else {
+    res.json({
+      code: 10024,
+      message:'未登陆'
+    })
+  }
+  
 })
 
+app.get('/', (req, res)=>{
+  res.send('Hello world');
+});
+
 app.listen(3000, () => {
-  console.log('成功')
+  console.log(`http://localhost:3000`)
 })
+
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
